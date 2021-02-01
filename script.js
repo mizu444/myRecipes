e24a890d52bbbe5ec6468586a9b21d7bd4a83314
Mizu Cuchty <m.cuchty@gmail.com>
2021-02-01 18:28:45 +0100

    show recipes and modal dynamically

--- script.js
+++ script.js
@@ -1,17 +1,59 @@
-const recipes = document.querySelectorAll(".recipe");
+const main = document.querySelector(".main");
+const recipesContainer = document.querySelector(".recipes-container");
+const recipesEl = document.querySelectorAll(".recipe");
 const modal = document.getElementById("modal");
 const close = document.getElementById("close");
+const modalTitle = document.getElementById("modalTitle");
+const modalIngredients = document.getElementById("modalIngredients");
+const modalDirections = document.getElementById("modalDirections");
 
-recipes.forEach((recipe, idx) => {
-    recipe.children[0].addEventListener("click", showModal)
-});
+showRecipes();
 
-function showModal() {
+async function showRecipes() {
+  const response = await fetch("/recipes.json");
+  const data = await response.json();
+
+  data.recipes.forEach(createRecipeEl);
+}
+
+function createRecipeEl(recipe) {
+  const recipeEl = document.createElement("div");
+  recipeEl.classList.add("recipe");
+
+  recipeEl.innerHTML = `
+    <h3 class="title">${recipe.title}</h3>
+    <img src="${recipe.image}"
+    alt=""/>
+  `;
+
+  recipesContainer.appendChild(recipeEl);
+
+  recipeEl.addEventListener("click", () => {
+    showModal(recipe);
+  });
+}
+
+function showModal(recipe) {
+  modalTitle.innerText = recipe.title;
+
+  const ingredientsEl = recipe.ingredients.map((ingredient) => {
+    const ingredientEl = document.createElement("li");
+    ingredientEl.innerText = ingredient;
+    return ingredientEl;
+  });
+
+  ingredientsEl.forEach((ingredientEl) =>
+    modalIngredients.appendChild(ingredientEl)
+  );
+
+  modalDirections.innerText = recipe.directions;
+
   modal.style.display = "block";
 }
 
-close.addEventListener("click", hideModal);
-
-function hideModal() {
-        modal.style.display = "none";
-}
+close.addEventListener("click", () => {
+  modal.style.display = "none";
+  modalTitle.innerHTML = "";
+  modalIngredients.innerHTML = "";
+  modalDirections.innerHTML = "";
+});
